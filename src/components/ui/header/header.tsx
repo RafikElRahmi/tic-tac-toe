d4424da60cd09092503c96a23df641ef50bef163
rafik rahmi <rafikrahmi10@gmail.com>
2024-10-24 11:02:24 +0100

    docs: ✏️ todo header task

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -2,7 +2,10 @@
 interface HeaderProps {
     computer: boolean;
 }
-function Header({computer }: HeaderProps) {
+function Header({ computer }: HeaderProps) {
+    /**TODO
+     * handle wins score
+     */
     return (
         <header className="absolute top-0 left-0  w-full h-[40px] bg-primary flex items-center justify-between px-2 sm:px-4">
             <div className="text-white">Player1</div>
